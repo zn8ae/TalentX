2a810069439bc0b9003c411b874e8a1e58e999a9
StevenQu <steven@d-172-25-203-215.dhcp.virginia.edu>
2017-04-24 13:25:16 -0400

    selenium1

--- models/models/test_front_end.py
+++ models/models/test_front_end.py
@@ -1,98 +1,65 @@
-# from selenium import webdriver
-# from django.test import TestCase
+from selenium import webdriver
+from django.test import TestCase
+import unittest
 
-# #driver = webdriver.Chrome('/usr/local/bin/chromedriver')
-# #driver = webdriver.Chrome('/usr/local/python/lib/python3.5/site-packages/chromedriver')
-# driver = webdriver.Chrome('/usr/local/bin/chromedriver')
-# #driver = webdriver.Firefox('/usr/bin/firefox')
-# url = "http://localhost:8003"
-# webdriver.get(url)
+driver = webdriver.Chrome()
 
-# #driver.get(url)
+#driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+url = 'localhost:8003'
 
-# # # temp = driver.find_element_by_name("signup")
-# # # #print find
+driver.get(url)
 
-# # #SignUp
-# # driver.find_element_by_name("signup").click()
-# # driver.find_element_by_id("id_username").send_keys("steven8790180")
-# # driver.find_element_by_id("id_email").send_keys("steven@gmail.com")
-# # driver.find_element_by_id("id_last_name").send_keys("qu")
-# # driver.find_element_by_id("id_first_name").send_keys("steven")
-# # driver.find_element_by_id("id_password").send_keys("stevenqu")
-# # driver.find_element_by_id("id_password2").send_keys("stevenqu")
-# # driver.find_element_by_id("submit").click()
-# # driver.find_element_by_id("success").click()
+class WebTestCase(unittest.TestCase):
 
-# # #LogIn
-# # driver.find_element_by_id("id_username").send_keys("steven8790180")
-# # driver.find_element_by_id("id_password").send_keys("stevenqu")
-# # driver.find_element_by_id("submit").click()
-
-# # #CreateSkill
-# # driver.find_element_by_id("id_skill").send_keys("fly")
-# # driver.find_element_by_id("id_price").send_keys("100")
-# # driver.find_element_by_id("id_desc").send_keys("flyflyfly")
-# # driver.find_element_by_id("submit").click()
-# # driver.find_element_by_id("success").click()
-
-# # #SignOut
-# # driver.find_element_by_name("signout").click()
-# # #If find signin, then success. Otherwise, Signout is unsuccessful.
-# # driver.find_element_by_name("signin")
-
-# # #Search
-# # driver.find_element_by_id("id_search").send_keys("fly")
-# # driver.find_element_by_id("submit").click()
+	def setup(self):
+		pass
 
 
-# class WebTestCase(TestCase):
+	def test_signup(self):
 
-# 	def setup(self):
-# 		pass
+		driver.find_element_by_name("signup").click()
+		driver.find_element_by_id("id_username").send_keys("steven4")
+		driver.find_element_by_id("id_email").send_keys("steven@gmail.com")
+		driver.find_element_by_id("id_last_name").send_keys("qu")
+		driver.find_element_by_id("id_first_name").send_keys("steven")
+		driver.find_element_by_id("id_password").send_keys("stevenqu")
+		driver.find_element_by_id("id_password2").send_keys("stevenqu")
+		driver.find_element_by_id("submit").click()
+		find = driver.find_element_by_id("success")
+		driver.find_element_by_id("success").click()
+		self.assertNotEquals(find, None)
 
-# 	def test_signup(self):
-# 		driver.find_element_by_name("signup").click()
-# 		driver.find_element_by_id("id_username").send_keys("steven")
-# 		driver.find_element_by_id("id_email").send_keys("steven@gmail.com")
-# 		driver.find_element_by_id("id_last_name").send_keys("qu")
-# 		driver.find_element_by_id("id_first_name").send_keys("steven")
-# 		driver.find_element_by_id("id_password").send_keys("stevenqu")
-# 		driver.find_element_by_id("id_password2").send_keys("stevenqu")
-# 		driver.find_element_by_id("submit").click()
-# 		find = driver.find_element_by_id("success")
-# 		driver.find_element_by_id("success").click()
-# 		self.assertNotEquals(find, None)
+	# def test_login(self):
+	# 	driver.find_element_by_name("signin").click()
+	# 	driver.find_element_by_id("id_username").send_keys("steven9")
+	# 	driver.find_element_by_id("id_password").send_keys("stevenqu")
+	# 	driver.find_element_by_id("submit").click()
+	# 	find = driver.find_element_by_name("signout")
+	# 	self.assertNotEquals(find, None)
 
-# 	def test_login(self):
-# 		driver.find_element_by_id("id_username").send_keys("steven")
-# 		driver.find_element_by_id("id_password").send_keys("stevenqu")
-# 		driver.find_element_by_id("submit").click()
-# 		find = driver.find_element_by_name("logout")
-# 		self.assertNotEquals(find, None)
+	# def test_create_skill(self):
+	# 	driver.find_element_by_id("id_skill").send_keys("fly")
+	# 	driver.find_element_by_id("id_price").send_keys("100")
+	# 	driver.find_element_by_id("id_desc").send_keys("flyflyfly")
+	# 	driver.find_element_by_id("submit").click()
+	# 	find = driver.find_element_by_id("success")
+	# 	driver.find_element_by_id("success").click()
+	# 	self.assertNotEquals(find, None)
 
-# 	def test_create_skill(self):
-# 		driver.find_element_by_id("id_skill").send_keys("fly")
-# 		driver.find_element_by_id("id_price").send_keys("100")
-# 		driver.find_element_by_id("id_desc").send_keys("flyflyfly")
-# 		driver.find_element_by_id("submit").click()
-# 		find = driver.find_element_by_id("success")
-# 		driver.find_element_by_id("success").click()
-# 		self.assertNotEquals(find, None)
+	# def test_singout(self):
+	# 	driver.find_element_by_name("signout").click()
+	# 	find = driver.find_element_by_name("signin")
+	# 	self.assertNotEquals(find, None)
 
-# 	def test_singout(self):
-# 		driver.find_element_by_name("signout").click()
-# 		find = driver.find_element_by_name("signin")
-# 		self.assertNotEquals(find, None)
+	# def test_search(self):
+	# 	driver.find_element_by_id("id_search").send_keys("fly")
+	# 	driver.find_element_by_id("submit").click()
+	# 	find = driver.find_element_by_name("view")
+	# 	self.assertNotEquals(find, None)
 
-# 	def test_search(self):
-# 		driver.find_element_by_id("id_search").send_keys("fly")
-# 		driver.find_element_by_id("submit").click()
-# 		find = driver.find_element_by_name("view")
-# 		self.assertNotEquals(find, None)
+	def tearDown(self):
+		pass #nothing to tear down
 
-# 	def tearDown(self):
-# 		pass #nothing to tear down
-
-
+if __name__ =='__main__':
+	unittest.main()
 
