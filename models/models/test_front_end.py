182b040dcd47c20416fe39516850e33c192ce61c
StevenQu <steven@StevendeMacBook-Pro.local>
2017-04-25 02:37:45 -0400

    selenium5

--- models/models/test_front_end.py
+++ models/models/test_front_end.py
@@ -2,13 +2,6 @@ from selenium import webdriver
 from django.test import TestCase
 import unittest
 
-driver = webdriver.Chrome()
-
-#driver = webdriver.Chrome('/usr/local/bin/chromedriver')
-url = 'localhost:8003'
-
-driver.get(url)
-
 class WebTestCase(unittest.TestCase):
 
 	def setup(self):
@@ -16,9 +9,11 @@ class WebTestCase(unittest.TestCase):
 
 
 	def test_signup(self):
-
+		driver = webdriver.Chrome()
+		url = 'localhost:8003'
+		driver.get(url)
 		driver.find_element_by_name("signup").click()
-		driver.find_element_by_id("id_username").send_keys("steven4")
+		driver.find_element_by_id("id_username").send_keys("steven08")
 		driver.find_element_by_id("id_email").send_keys("steven@gmail.com")
 		driver.find_element_by_id("id_last_name").send_keys("qu")
 		driver.find_element_by_id("id_first_name").send_keys("steven")
@@ -26,36 +21,56 @@ class WebTestCase(unittest.TestCase):
 		driver.find_element_by_id("id_password2").send_keys("stevenqu")
 		driver.find_element_by_id("submit").click()
 		find = driver.find_element_by_id("success")
+		self.assertNotEquals(find, None)
+
+	def test_login(self):
+		driver = webdriver.Chrome()
+		url = 'localhost:8003'
+		driver.get(url)
+		driver.find_element_by_name("signin").click()
+		driver.find_element_by_id("id_username").send_keys("steven03")
+		driver.find_element_by_id("id_password").send_keys("stevenqu")
+		driver.find_element_by_id("submit").click()
+		find = driver.find_element_by_id("id_skill")
+		self.assertNotEquals(find, None)
+
+	def test_create_skill(self):
+		driver = webdriver.Chrome()
+		url = 'localhost:8003'
+		driver.get(url)
+		driver.find_element_by_name("create").click()
+		driver.find_element_by_id("id_username").send_keys("steven03")
+		driver.find_element_by_id("id_password").send_keys("stevenqu")
+		driver.find_element_by_id("submit").click()
+		driver.find_element_by_id("id_skill").send_keys("fly")
+		driver.find_element_by_id("id_price").send_keys("100")
+		driver.find_element_by_id("id_desc").send_keys("flyflyfly")
+		driver.find_element_by_id("submit").click()
+		find = driver.find_element_by_id("success")
 		driver.find_element_by_id("success").click()
 		self.assertNotEquals(find, None)
 
-	# def test_login(self):
-	# 	driver.find_element_by_name("signin").click()
-	# 	driver.find_element_by_id("id_username").send_keys("steven9")
-	# 	driver.find_element_by_id("id_password").send_keys("stevenqu")
-	# 	driver.find_element_by_id("submit").click()
-	# 	find = driver.find_element_by_name("signout")
-	# 	self.assertNotEquals(find, None)
 
-	# def test_create_skill(self):
-	# 	driver.find_element_by_id("id_skill").send_keys("fly")
-	# 	driver.find_element_by_id("id_price").send_keys("100")
-	# 	driver.find_element_by_id("id_desc").send_keys("flyflyfly")
-	# 	driver.find_element_by_id("submit").click()
-	# 	find = driver.find_element_by_id("success")
-	# 	driver.find_element_by_id("success").click()
-	# 	self.assertNotEquals(find, None)
+	def test_singout(self):
+		driver = webdriver.Chrome()
+		url = 'localhost:8003'
+		driver.get(url)
+		driver.find_element_by_name("signin").click()
+		driver.find_element_by_id("id_username").send_keys("steven03")
+		driver.find_element_by_id("id_password").send_keys("stevenqu")
+		driver.find_element_by_id("submit").click()
+		driver.find_element_by_name("signout").click()
+		find = driver.find_element_by_name("signin")
+		self.assertNotEquals(find, None)
 
-	# def test_singout(self):
-	# 	driver.find_element_by_name("signout").click()
-	# 	find = driver.find_element_by_name("signin")
-	# 	self.assertNotEquals(find, None)
-
-	# def test_search(self):
-	# 	driver.find_element_by_id("id_search").send_keys("fly")
-	# 	driver.find_element_by_id("submit").click()
-	# 	find = driver.find_element_by_name("view")
-	# 	self.assertNotEquals(find, None)
+	def test_search(self):
+		driver = webdriver.Chrome()
+		url = 'localhost:8003'
+		driver.get(url)
+		driver.find_element_by_id("id_search").send_keys("fly")
+		driver.find_element_by_id("submit").click()
+		find = driver.find_element_by_name("view")
+		self.assertNotEquals(find, None)
 
 	def tearDown(self):
 		pass #nothing to tear down
